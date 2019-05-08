74521187b32531789a1e71c489ae3f2b67a673a3
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2019-05-08 14:39:01 +0200

    Einstellungen als xml Datei hinzugefügt

--- SPS-Starter/MainWindow.xaml.cs
+++ SPS-Starter/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace SPS_Starter
 {
@@ -20,17 +21,45 @@ namespace SPS_Starter
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string Ordner_Logo;
+        string Ordner_TiaPortal;
+        string Ordner_TwinCAT;
+
         public MainWindow()
         {
             InitializeComponent();
+            EinstellungenLesen("Einstellungen.xml");
         }
 
         public void ProjekteLesen()
         {
+
+
             Projekte_Logo_Lesen();
             Projekte_TiaPortal_Lesen();
             Projekte_TwinCAT_Lesen();
         }
+
+
+
+        public void EinstellungenLesen(string fileName)
+        {
+            XDocument doc = XDocument.Load(fileName);
+
+            foreach (XElement el in doc.Root.Elements())
+            {
+                switch (el.Name.LocalName)
+                {
+                    case "Logo": Ordner_Logo = el.Value.Trim(); break;
+                    case "TiaPortal": Ordner_TiaPortal = el.Value.Trim(); break;
+                    case "TwinCAT": Ordner_TwinCAT = el.Value.Trim(); break;
+                    default: break;
+                }
+            }
+        }
+
+
 
     }
 }
